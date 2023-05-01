f6c2f51fd077a55fd2368494cddbad95270047a3
Marco Dehmel <81867567+codetist@users.noreply.github.com>
2023-05-01 20:49:44 +0200

    Updated dependencies to jakarta namespace

--- quarkusapilambda/src/main/java/de/codemonaut/aws/note/Note.java
+++ quarkusapilambda/src/main/java/de/codemonaut/aws/note/Note.java
@@ -2,8 +2,8 @@ package de.codemonaut.aws.note;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class Note {
